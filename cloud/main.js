ec88f10cec9f47d971e18e60039ac200570dbf3f
Julien <j.paroche@gmail.com>
2015-07-26 16:55:00 +0200

    4 characters check on verif code and string errors
    • Add check to ensure verification code sent by SMS is exactly 4 characters long
    • Change error data from JSON to simple string error code
    • Change `hello` Cloud Function to `slove`
    • Add easy switch for Twilio to go from test to live

--- cloud/main.js
+++ cloud/main.js
@@ -1,32 +1,47 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+Parse.Cloud.define('slove', function(request, response) {
+  response.success('You\'ve been sloved!');
 });
 
 /**
- * Twilio test
+ * Twilio
  */
+var twilioTestMode = true;
+
+var testSid = 'AC67aa36effde530903ec7d9a2b11b9498';
+var testTok = '0b508f57260346c4b909d3f4e063c3b3';
+var liveSid = 'AC7848a69b13ee905acf3aa3fc00a32270';
+var liveTok = '30d9b2319348852525081e14ce693749';
+
 var twilio = require('twilio')(
-  'AC67aa36effde530903ec7d9a2b11b9498',
-  '0b508f57260346c4b909d3f4e063c3b3'
+  twilioTestMode ? testSid : liveSid,
+  twilioTestMode ? testTok : liveTok
 );
 
 // Parse Cloud Function
 Parse.Cloud.define('sendPhoneCode', function(request, response) {
   var phoneNumber = request.params.phoneNumber;
   var query = new Parse.Query('_User');
-  query.equalTo('phoneNumber', phoneNumber);
 
+  query.equalTo('phoneNumber', phoneNumber);
   query.find().then(function(queryData) {
     if (queryData.length > 0) {
-      response.error({ status: 'phone_number_already_used' });
+      response.error('phone_number_already_used');
     }
     else {
-      var verificationCode = Math.floor(Math.random() * 9999);
+      var verificationCode = Math.floor(Math.random() * 9999).toString();
+      // Ensures that the code is exactly 4 characters long
+      if (verificationCode.length > 4) {
+        verificationCode = verificationCode.slice(0, 4);
+      }
+      while (verificationCode.length < 4) {
+        verificationCode = '0' + verificationCode;
+      }
+
+      // Save the verificationCode for the user who requested the validation
       var user = Parse.User.current();
-      user.set('phoneVerificationCode', verificationCode.toString());
+      user.set('phoneVerificationCode', verificationCode);
       user.save();
 
       twilio.sendSms({
@@ -40,8 +55,7 @@ Parse.Cloud.define('sendPhoneCode', function(request, response) {
           response.success(responseData);
         }
         else {
-          errorData.status = 'failed_to_send';
-          response.error(errorData);
+          response.error('failed_to_send');
         }
       });
     }
@@ -51,6 +65,7 @@ Parse.Cloud.define('sendPhoneCode', function(request, response) {
 Parse.Cloud.define('confirmPhoneCode', function(request, response) {
   var user = Parse.User.current();
   var verificationCode = user.get('phoneVerificationCode');
+
   if (verificationCode === request.params.phoneVerificationCode) {
     user.set('phoneNumber', request.params.phoneNumber);
     user.save();
